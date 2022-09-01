432865521f0f06a79583c243cb229bcce88a663d
Илья Силкин <zerolar@gmail.com>
2022-09-01 14:08:37 +0300

    Added search for posts, tags and people

---
+++ Blog/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using Blog.Data.Repository;
+using Blog.Data.UnitOfWork;
+using Blog.Models.DB;
+using Microsoft.AspNetCore.Mvc;
+namespace Blog.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SearchController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchPosts(string search)
+        {
+            ViewData["Search"] = search;
+
+            var repository = _unitOfWork.GetRepository<Article>() as ArticleRepository;
+            IEnumerable<Article> articles = await repository.GetAllArticles();
+
+            var articleList = articles.Where(x => x.Title.ToLower().Contains(search.ToLower()));
+
+            return View(articleList);
+        }
+    }
+}
---
+++ Blog/ViewModels/SearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Models.DB;
+
+namespace Blog.ViewModels
+{
+    public class SearchViewModel
+    {
+        public List<Article> Articles { get; set; }
+        public List<Tag> Tags { get; set; }
+        public List<ApplicationUser> Users { get; set; }
+    }
+}
